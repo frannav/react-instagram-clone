d6f83564fe4dbab849b123f941be67f3f71d2bc3
Fran Nav <frannavmart@gmail.com>
2020-10-07 18:05:04 +0100

    Added Logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotoCards } from './components/ListOfPhotoCards';
 import { GlobalStyle } from './GlobalStyles';
+import { Logo } from './components/Logo';
 
 export const App = () => (
     <>
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
         <ListOfPhotoCards />
     </>
