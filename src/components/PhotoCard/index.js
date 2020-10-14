0f985b44158de238cf779711d9e601126aaf602c
Fran Nav <frannavmart@gmail.com>
2020-10-14 16:39:37 +0100

    Refactor Extract useLocalStorage´s custom hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,18 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { ImgWrapper, Img, Button, Article } from './styles';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
+
+
+
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const element = useRef(null)
     const [show, setShow] = useState(false)
     const key = `like-${id}`
-    const [liked, setLiked] = useState(() => {
-        try {
-           const like = window.localStorage.getItem(key)
-           return JSON.parse(like)
-        } catch (e) {
-            return false
-        }
-    })
-    const element = useRef(null)
+    const [liked, setLiked] = useLocalStorage(key, false)
 
 
     console.log(liked)
@@ -39,14 +36,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 
     const Icon = liked ? MdFavorite : MdFavoriteBorder
 
-    const setLocalStorage = (value) => {
-        try {
-            window.localStorage.setItem(key, value)
-            setLiked(value)
-        } catch (e) {
-            console.err(e)
-        }
-    }
+
 
     return (
         <Article ref={element}>
@@ -60,7 +50,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
                         </ImgWrapper>
                     </a>
 
-                    <Button onClick={() => setLocalStorage(!liked)}>
+                    <Button onClick={() => setLiked(!liked)}>
                         <Icon size='32px'/> {likes} likes!
                     </Button>
                </>
