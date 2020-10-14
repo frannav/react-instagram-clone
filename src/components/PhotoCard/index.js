ffcf45eeff06a855b2126a231d2568a304aac517
Fran Nav <frannavmart@gmail.com>
2020-10-14 16:45:48 +0100

    Refactor Extract useNearScreen´s custom hook

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useRef, useState} from 'react';
 import { ImgWrapper, Img, Button, Article } from './styles';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useNearScreen } from '../../hooks/useNearScreen';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
@@ -9,30 +10,14 @@ const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_8
 
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-    const element = useRef(null)
-    const [show, setShow] = useState(false)
+    const [show, element] = useNearScreen()
     const key = `like-${id}`
     const [liked, setLiked] = useLocalStorage(key, false)
 
 
     console.log(liked)
 
-    useEffect(function () {
-        Promise.resolve(
-            typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver 
-            : import('intersection-observer')
-        ).then(() => {
-            const observer = new window.IntersectionObserver(function(entries) {
-                const { isIntersecting } = entries[0]
-                console.log(isIntersecting)
-                if(isIntersecting) {
-                    setShow(true)
-                    observer.disconnect()
-                }
-            })
-            observer.observe(element.current)
-        })
-    }, [element])
+
 
     const Icon = liked ? MdFavorite : MdFavoriteBorder
 
